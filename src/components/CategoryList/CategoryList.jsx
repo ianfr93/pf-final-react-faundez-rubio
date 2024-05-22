0f1c0728da101155d4053c86305138d121613316
ianfr93 <albitox10@gmail.com>
2024-05-22 01:49:28 -0400

    cambios

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,34 +1,36 @@
-import "./CategoryList.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { List, ListItem, ListItemText } from "@mui/material";
+import "./CategoryList.css";
 
 function CategoryList() {
   return (
-    <nav>
-      <ul className="category-list">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/category/gamer-zone">Gamer Zone</Link>
-        </li>
-        <li>
-          <Link to="/category/arma-tu-pc">Arma tu PC</Link>
-        </li>
-        <li>
-          <Link to="/category/tiendas">Tiendas</Link>
-        </li>
-        <li>
-          <Link to="/category/servicios">Servicios</Link>
-        </li>
-        <li>
-          <Link to="/category/seguir-mi-pedido">Seguir mi pedido</Link>
-        </li>
-        <li>
-          <Link to="/category/contacto">Contacto</Link>
-        </li>
-      </ul>
-    </nav>
+    <List>
+      <ListItem component={Link} to="/">
+        <ListItemText primary="Home" />
+      </ListItem>
+      <ListItem component={Link} to="/category/gamer-zone">
+        <ListItemText primary="Gamer Zone" />
+      </ListItem>
+      <ListItem component={Link} to="/category/arma-tu-pc">
+        <ListItemText primary="Arma tu PC" />
+      </ListItem>
+      <ListItem component={Link} to="/category/tiendas">
+        <ListItemText primary="Tiendas" />
+      </ListItem>
+      <ListItem component={Link} to="/category/servicios">
+        <ListItemText primary="Servicios" />
+      </ListItem>
+      <ListItem component={Link} to="/category/seguir-mi-pedido">
+        <ListItemText primary="Seguir mi pedido" />
+      </ListItem>
+      <ListItem component={Link} to="/category/contacto">
+        <ListItemText primary="Contacto" />
+      </ListItem>
+      <ListItem component={Link} to="/about">
+        <ListItemText primary="About" />
+      </ListItem>
+    </List>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,31 +1,48 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import CategoryList from '../CategoryList/CategoryList'; 
+import CategoryList from '../CategoryList/CategoryList';
 import CartWidget from "../CartWidget/CartWidget";
 import './NavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'; 
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { AppBar, Toolbar, IconButton, Drawer, Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Brand from '../Brand/Brand';
 
 function NavBar() {
-  const [showMenu, setShowMenu] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => () => {
+    setDrawerOpen(open);
+  };
 
   return (
-    <div className='navbar__wrapper'>
-      <div className='menu-toggle' onClick={() => setShowMenu(!showMenu)}>
-        <div className='bar'></div>
-        <div className='bar'></div>
-        <div className='bar'></div>
-      </div>
-      {showMenu ? <CategoryListMobile /> : <CategoryList />}
-      <div className="right-items">
-        <li className="login-item">
-          <Link to="/login" className="login-link">
-            <FontAwesomeIcon icon={faGoogle} className="user-icon" />
-            <span>Ingresar</span>
-          </Link>
-        </li>
-        <CartWidget />
-      </div>
+    <div className="navbar__wrapper">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)} sx={{ display: { xs: 'block', md: 'none' } }}>
+            <MenuIcon />
+          </IconButton>
+          <Brand />
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'block' } }}>
+            <CategoryList />
+          </Box>
+          <Box className="right-items">
+            <li className="login-item">
+              <Link to="/login" className="login-link">
+                <FontAwesomeIcon icon={faGoogle} className="user-icon" />
+                <span>Ingresar</span>
+              </Link>
+            </li>
+            <CartWidget />
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+          <CategoryList />
+        </Box>
+      </Drawer>
     </div>
   );
 }
