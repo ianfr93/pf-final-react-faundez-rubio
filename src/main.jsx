a4775a590d442235c8e1a28b0030b3c3ed920e1d
ianfr93 <albitox10@gmail.com>
2024-05-21 23:11:52 -0400

    cambios jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,13 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './config/theme';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
