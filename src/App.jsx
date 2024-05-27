a03aa68867fa790240189a387b4cb148c54010d4
ianfr93 <albitox10@gmail.com>
2024-05-27 00:23:30 -0400

    context

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import SearchResults from "./components/SearchResults/SearchResults";
 import useProducts from "./hooks/useProducts";
-import ImageCarousel from "./components/ImageCarousel/ImageCarousel"; 
-import { CartProvider } from './context/CartContext'; 
+import ImageCarousel from "./components/ImageCarousel/ImageCarousel";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   const { products, isLoading } = useProducts();
@@ -15,24 +15,24 @@ function App() {
   if (isLoading) return <h1>Cargando...</h1>;
 
   return (
-    <Layout>
-      {location.pathname === "/" && <ImageCarousel />} {/* Solo muestra el carrusel en la ruta principal */}
-      <Routes>
-        <Route path="/" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
-        <Route path="/category/:id" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
-        <Route path="/product/:id" element={<ItemDetailContainer products={products} />} />
-        <Route path="/search" element={<SearchResults />} />
-      </Routes>
-    </Layout>
+    <CartProvider>
+      <Layout>
+        {location.pathname === "/" && <ImageCarousel />}
+        <Routes>
+          <Route path="/" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
+          <Route path="/category/:id" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
+          <Route path="/product/:id" element={<ItemDetailContainer products={products} />} />
+          <Route path="/search" element={<SearchResults />} />
+        </Routes>
+      </Layout>
+    </CartProvider>
   );
 }
 
 function AppWrapper() {
   return (
     <Router>
-      <CartProvider> {/* Envuelve tu aplicación con CartProvider */}
-        <App />
-      </CartProvider>
+      <App />
     </Router>
   );
 }
