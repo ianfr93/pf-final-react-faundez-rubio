fef0d902755033424c86486ea9aadd4dec70d055
ianfr93 <albitox10@gmail.com>
2024-05-26 22:16:51 -0400

    cambios app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
@@ -9,22 +9,29 @@ import ImageCarousel from "./components/ImageCarousel/ImageCarousel"; // Importa
 
 function App() {
   const { products, isLoading } = useProducts();
+  const location = useLocation();
 
   if (isLoading) return <h1>Cargando...</h1>;
 
   return (
+    <Layout>
+      {location.pathname === "/" && <ImageCarousel />} {/* Solo muestra el carrusel en la ruta principal */}
+      <Routes>
+        <Route path="/" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
+        <Route path="/category/:id" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
+        <Route path="/product/:id" element={<ItemDetailContainer products={products} />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+function AppWrapper() {
+  return (
     <Router>
-      <Layout>
-        <ImageCarousel /> {/* Agrega el carrusel aquí */}
-        <Routes>
-          <Route path="/" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
-          <Route path="/category/:id" element={<ItemListContainer saludo="¡Bienvenido a nuestra tienda!" products={products} />} />
-          <Route path="/product/:id" element={<ItemDetailContainer products={products} />} />
-          <Route path="/search" element={<SearchResults />} />
-        </Routes>
-      </Layout>
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
