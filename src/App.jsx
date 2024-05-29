02dc4a8020f569249119c3661146629da8db832b
ianfr93 <albitox10@gmail.com>
2024-05-29 14:53:15 -0400

    json

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
@@ -7,21 +7,12 @@ import SearchResults from "./components/SearchResults/SearchResults";
 import useProducts from "./hooks/useProducts";
 import ImageCarousel from "./components/ImageCarousel/ImageCarousel";
 import { CartProvider } from "./context/CartContext";
-import CartWidget from "./components/CartWidget/CartWidget";
-import AuthModal from "./components/AuthModal/AuthModal"; 
+import CartWidget from "./components/CartWidget/CartWidget"; 
+
 
 function App() {
   const { products, isLoading } = useProducts();
   const location = useLocation();
-  const [authModalOpen, setAuthModalOpen] = useState(false);
-
-  const handleAuthModalOpen = () => {
-    setAuthModalOpen(true);
-  };
-
-  const handleAuthModalClose = () => {
-    setAuthModalOpen(false);
-  };
 
   if (isLoading) return <h1>Cargando...</h1>;
 
@@ -36,7 +27,6 @@ function App() {
           <Route path="/search" element={<SearchResults />} />
         </Routes>
         <CartWidget /> {/* Asegúrate de que CartWidget se renderice aquí */}
-        <AuthModal open={authModalOpen} handleClose={handleAuthModalClose} /> {/* Renderiza AuthModal */}
       </Layout>
     </CartProvider>
   );
