3091ef0178a42c8cefd5401a873e9628c9f602b0
ianfr93 <albitox10@gmail.com>
2024-05-29 15:46:53 -0400

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from ''
-import react from '@js/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://js.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
