b2b14db29ac621857c8e3da84f903bc6d17e542d
ianfr93 <albitox10@gmail.com>
2024-05-28 21:26:28 -0400

    nuevo codigo

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,20 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { getProducts } from "../mock/asyncMock";
+import { useEffect, useState } from "react";
+import { getProducts } from "../services/productService"; 
 
-export default function useProducts(){
-    const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [showSaludo, setShowSaludo] = useState(false); // Estado para controlar la visibilidad del saludo
-  
-    useEffect(() => {
-      getProducts()
-        .then((data) => {
-          setProducts(data);
-          setShowSaludo(true); // Mostrar el saludo después de cargar los productos
-        })
-        .catch((error) => console.error("Error al obtener productos:", error))
-        .finally(() => setIsLoading(false));
-    }, []);
+export default function useProducts() {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [showSaludo, setShowSaludo] = useState(false); 
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+        setShowSaludo(true); 
+      } catch (error) {
+        console.error("Error al obtener productos:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return { products, isLoading };
 }
