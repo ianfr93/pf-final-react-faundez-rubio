a90e53a8055c7975dc1b20b51a9ddede293ea361
ianfr93 <albitox10@gmail.com>
2024-05-28 20:57:18 -0400

    firebase.config.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// src/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiQ1yd83yhjd0jEGhkKkiIJ4WnPPVSGe0",
+  authDomain: "ecommerce-react-54120-6b43a.firebaseapp.com",
+  projectId: "ecommerce-react-54120-6b43a",
+  storageBucket: "ecommerce-react-54120-6b43a.appspot.com",
+  messagingSenderId: "476630605163",
+  appId: "1:476630605163:web:e6c9583d37ed6fc146802e"
+};
+
+// Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore
+const db = getFirestore(app);
+
+export { db };
